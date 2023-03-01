3d05ab65b5aa670be15582173750f00142497aac
RyanNealon2 <112398156+RyanNealon2@users.noreply.github.com>
2023-02-28 19:41:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import pandas as pd
 
 sc = ScoreCard('4HYJoiEQMYhtUcoVK9K7dFmJNmDArviXBizfvdzi')
 for college in sc.search('Marist'):
-   dict = {'College Name': college.name, 'state': college.location['state'], 'Tuition': college.}
+    dict = {'College Name': college.data['latest.school.name'], 'state': college.data['latest.school.state'],'Zip': college.data['latest.school.zip'], 'Average Price': college.data['latest.cost.avg_net_price.private']}
 dictionaryDataFrame = pd.DataFrame(dict, index=[0])
 
 
 with pd.ExcelWriter('output.xlsx') as engine:
     dictionaryDataFrame.to_excel(excel_writer=engine, sheet_name='CollegeData', index=False)
+
 
 
 
